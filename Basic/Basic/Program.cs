3fa9bc6ca9692ba153658e18159572db5a899a4a
xeeraki <shafai86@gmail.com>
2021-07-30 08:55:28 +0200

    basic class tutorial just for refreshing

---
+++ Basic/Basic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Basic
+{
+    public class Person
+    {
+        private string Name;
+
+        public Person()
+        {
+            
+        }
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to,Name);
+        }
+
+        public string SetName(string name)
+        {
+            this.Name = name;
+            return Name;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetName("Mosh");
+            person.Introduce("Adam");
+        }
+    }
+}
