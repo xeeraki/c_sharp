55737474ba3ed56fc90b8e54cf94d23403f9fc05
xeeraki <shafai86@gmail.com>
2021-08-01 13:21:26 +0200

    access modifier

---
+++ Basic/Basic/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic { 
+    public class Person
+{
+    private string Name;
+    public int Id;
+        private DateTime _birthdate;
+
+    public Person()
+    {
+
+    }
+
+    public void Introduce(string to)
+    {
+        Console.WriteLine("Hi {0}, I am {1}", to, Name);
+    }
+
+        internal DateTime SetBirthdate(DateTime birthdate)
+        {
+           return _birthdate = birthdate;
+        }
+
+        public string SetName(string name)
+    {
+        this.Name = name;
+        return Name;
+    }
+}
+}
