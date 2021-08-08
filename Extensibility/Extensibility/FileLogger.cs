c9e413b5478516c8a6d3c474cd5d77de3724a2a2
xeeraki <shafai86@gmail.com>
2021-08-08 10:33:41 +0200

    intermediate c# learning about composite pattern using interafce to achive loosly coupled system

---
+++ Extensibility/Extensibility/FileLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensibility
+{
+    class FileLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Extensibility/Extensibility/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Extensibility 
+
+{
+
+    public class DbMigrator
+        
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+        _logger = logger;
+
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("Migrating started at {0}" + DateTime.Now);
+            
+            _logger.LogInfo("Migrating finished at {0}" + DateTime.Now);
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbMigrator = new DbMigrator(new ConsoleLogger());
+
+            dbMigrator.Migrate();
+            
+        }
+    }
+}
