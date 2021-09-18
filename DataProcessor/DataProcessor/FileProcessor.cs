05c74e5e7b924c7c9e01b20c96ba9183456ae439
xeeraki <shafai86@gmail.com>
2021-09-18 12:20:48 +0200

    data processor

---
+++ DataProcessor/DataProcessor/FileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        public string InputFilePath { get; set; }
+    }
+
+    public FileProceesser(string filepath)
+    {
+        InputFilePath = filepath;
+    }
+    
+}
---
+++ overflow/overflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<string>();
+            numbers.Add("Hello world");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+            Console.WriteLine(numbers.Count);
+            var dateTime = new DateTime(2021, 08, 28);
+            Console.WriteLine(dateTime);
+
+            var now = DateTime.Now;
+            var next = DateTime.UtcNow;
+
+            var span = now - next;
+
+            Console.WriteLine("span are\t" + span);
+        }
+    }
+}
