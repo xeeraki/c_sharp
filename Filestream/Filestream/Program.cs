2bb46197839ced8752031a5e02121d8a46baed62
xeeraki <shafai86@gmail.com>
2021-09-21 02:22:57 +0200

    feature(System.IO):file directories

---
+++ Filestream/Filestream/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Filestream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\";
+            var dir = new FileProcessor();
+
+            dir.readDir(filePath);
+         
+           
+
+ 
+        }
+    }
+
+
+    public class FileProcessor { 
+
+       public void readDir(string filePath)
+        {
+            var dir = Directory.GetDirectories(filePath);
+
+            foreach(var x in dir)
+            {
+                Console.WriteLine("Processed" +x);
+            }
+
+        }
+       
+
+    }
+}
